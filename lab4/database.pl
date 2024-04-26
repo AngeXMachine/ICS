08d4526f8c98c2bb798952351acf983b04db87d0
AngeXMachine <116073127+AngeXMachine@users.noreply.github.com>
2024-04-26 16:46:26 +0800

    Add files via upload

---
+++ lab4/database.pl
@@ -0,0 +1,158 @@
+/** DRAMA */
+film('The Green Mile','Frank Darabont',1999,'drama', 'others',  188,'USA','Tom Hanks',8.7,719).
+film('Bang Bang! You\'re Dead', 'Guy Ferland', 2002, 'drama', 'others',  87, 'Canada', 'Ben Foster', 7.7, 16).
+film('Dogville', 'Lars von Trier', 2003, 'drama', 'others', 178, 'Denmark', 'Nicole Kidman', 7.7, 45).
+film('From a Distance', 'Ton Kayne', 2011, 'drama', 'others', 100, 'USA', 'Adrien Brody', 7.9, 30).
+film('Schindler\'s List', 'Steven Spielberg', 1993, 'drama', 'others', 195, 'USA', 'Adrien Brody', 8.4, 259).
+film('Requiem for a Dream', 'Darren Aronofsky', 2000, 'drama', 'others', 102, 'USA', 'Jared Leto', 7.9, 520).
+film('Biutiful', 'Alejandro Gonzalez Inarritu', 2010, 'drama', 'others', 148, 'Spain', 'Javier Bardem', 7.6, 12).
+film('Black Swan', 'Darren Aronofsky', 2010, 'drama', 'others', 108, 'USA', 'Natalie Portman', 7.7, 248).
+film('Gladiator', 'Ridley Scott', 2000, 'drama', 'others', 155, 'USA', 'Russell Crowe', 8.1, 552).
+film('The Pianist', 'Roman Polanski', 2002, 'drama', 'others', 150, 'Poland', 'Adrien Brody', 8.3, 410).
+film('Stalker', 'Andrei Tarkovsky', 1979, 'drama', 'others', 161, 'Russia', 'Alexander Kaidanovsky', 8.1, 290).
+film('Come and See', 'Elem Klimov', 1985, 'drama', 'others', 142, 'Russia', 'Alesya Krasnova', 8.1, 82).
+film('Moscow Does Not Believe in Tears', 'Vladimir Menshov', 1980, 'drama', 'others', 150, 'Russia', 'Vera Alentova', 8.4, 46).
+
+
+/** COMEDY */
+film('Sexmission', 'Juliusz Machulski', 1984, 'comedy', 'others', 118, 'Poland', 'Jerzy Stuhr', 7.9, 420).
+film('Forrest Gump', 'Robert Zemeckis', 1994, 'comedy', 'others', 144, 'USA', 'Tom Hanks', 8.6, 697).
+film('The Hangover', 'Todd Phillips', 2009, 'comedy', 'others', 100, 'USA', 'Bradley Cooper', 7.3, 537).
+film('The Intouchables', 'Olivier Nakache', 2011, 'comedy', 'others', 112, 'France', 'Francois Cluzet', 8.7, 393).
+film('The Truman Show', 'Peter Weir', 1998, 'comedy', 'others', 103, 'USA', 'Jim Carrey', 7.4, 383).
+film('Home Alone', 'Chris Columbus', 1990, 'comedy', 'others', 103, 'USA', 'Macaulay Culkin', 7.1, 297).
+film('The Devil Wears Prada', 'David Frankel', 2006, 'comedy', 'others', 109, 'USA', 'Meryl Streep', 6.9, 227).
+film('Ivan Vasilievich: Back to the Future', 'Leonid Gaidai', 1973, 'comedy', 'others', 93, 'Russia', 'Yuri Yakovlev', 8.5, 142).
+film('Operation Y and Other Shurik\'s Adventures', 'Leonid Gaidai', 1965, 'comedy', 'others', 95, 'Russia', 'Alexei Smirnov', 8.6, 191).
+film('Gentlemen of Fortune', 'Alexander Sery', 1971, 'comedy', 'others', 88, 'Russia', 'Yevgeny Leonov', 8.6, 180).
+
+
+/** ADVENTURE */
+film('The Lord of the Rings: The Fellowship of the Ring', 'Peter Jackson', 2001, 'adventure', 'fantasy', 178, 'New Zealand', 'Elijah Wood', 8.1, 503).
+film('Sherlock Holmes', 'Guy Ritchie', 2009, 'adventure', 'family', 128, 'Nomcy', 'Robert Downey Jr', 7.8, 317).
+film('Harry Potter and the Philosopher\'s Stone', 'Chris Columbus', 2001, 'adventure', 'fantasy', 152, 'United Kingdom', 'Daniel Radcliffe', 7.4, 269).
+film('The Hobbit: An Unexpected Journey', 'Peter Jackson', 2012, 'adventure', 'fantasy', 169, 'New Zealand', 'Martin Freeman', 7.7, 269).
+film('Pirates of the Caribbean: Dead Man\'s Chest', 'Gore Verbinski', 2006, 'adventure', 'fantasy', 150, 'USA', 'Johnny Depp', 7.8, 295).
+film('The Matrix', 'Andy Wachowski', 1999, 'adventure', 'sci-fi', 136, 'USA', 'Keanu Reeves', 7.6, 629).
+film('Back to the Future', 'Robert Zemeckis', 1985, 'adventure', 'sci-fi', 116, 'USA', 'Michael J. Fox', 7.3, 265).
+film('The Witcher', 'Marek Brodzki', 2001, 'adventure', 'fantasy', 130, 'Poland', 'Michal Zebrowski', 3.9, 37).
+film('Star Wars: Episode IV - A New Hope', 'George Lucas', 1977, 'adventure', 'sci-fi', 121, 'USA', 'Mark Hamill', 8.0, 199).
+film('Ivan Vasilievich: Back to the Future', 'Leonid Gaidai', 1973, 'adventure', 'comedy', 93, 'Russia', 'Yuri Yakovlev', 8.5, 142).
+film('Operation Y and Other Shurik\'s Adventures', 'Leonid Gaidai', 1965, 'adventure', 'comedy', 95, 'Russia', 'Alexei Smirnov', 8.6, 191).
+film('Gentlemen of Fortune', 'Alexander Sery', 1971, 'adventure', 'comedy', 88, 'Russia', 'Yevgeny Leonov', 8.6, 180).
+
+
+/** THRILLER */
+film('The Silence of the Lambs', 'Jonathan Demme', 1991, 'thriller', 'others', 118, 'USA', 'Anthony Hopkins', 8.2, 474).
+film('The Da Vinci Code', 'Ron Howard', 2006, 'thriller', 'others', 149, 'USA', 'Tom Hanks', 6.6, 447).
+film('The Sixth Sense', 'M. Night Shyamalan', 1999, 'thriller', 'others', 107, 'USA', 'Bruce Willis', 7.9, 482).
+film('Fight Club', 'David Fincher', 1999, 'thriller', 'others', 139, 'Nomcy', 'Brad Pitt', 8.3, 426).
+film('Se7en', 'David Fincher', 1995, 'thriller', 'others', 127, 'USA', 'Morgan Freeman', 8.3, 409).
+film('Shutter Island', 'Martin Scorsese', 2010, 'thriller', 'others', 138, 'USA', 'Leonardo DiCaprio', 8.1, 374).
+film('The Devil\'s Advocate', 'Taylor Hackford', 1997, 'thriller', 'others', 144, 'Nomcy', 'Keanu Reeves', 8.0, 317).
+film('American Psycho', 'Mary Harron', 2000, 'thriller', 'others', 'USA', 101, 'Christian Bale', 7.3, 105).
+film('The Others', 'Alejandro Amenabar', 2001, 'thriller', 'others', 'France', 101, 'Nicole Kidman', 7.4, 166).
+film('Stalingrad', 'Fedor Bondarchuk', 2013, 'thriller', 'others', 131, 'Russia', 'Thomas Kretschmann', 5.6, 182).
+film('Night Watch', 'Timur Bekmambetov', 2004, 'thriller', 'others', 114, 'Russia', 'Konstantin Khabenskiy', 6.4, 93).
+film('Day Watch', 'Timur Bekmambetov', 2006, 'thriller', 'others', 132, 'Russia', 'Konstantin Khabenskiy', 6.5, 81).
+
+/** ANIMATION */
+film('Shrek', 'Andrew Adamson', 2001, 'animation', 'others', 90, 'USA', 'Mike Myers', 7.8, 604).
+film('Ice Age', 'Chris Wedge', 2002, 'animation', 'others', 81, 'USA', 'Cezary Pazura', 7.6, 359).
+film('The Lion King', 'Rob Minkoff', 1994, 'animation', 'others', 89, 'USA', 'Matthew Broderick', 8.3, 381).
+film('Madagascar', 'Tom McGrath', 2005, 'animation', 'others', 86, 'USA', 'Ben Stiller', 7.5, 290).
+film('Up', 'Pete Docter', 2009, 'animation', 'others', 96, 'USA', 'Edward Asner', 7.4, 161).
+film('Finding Nemo', 'Andrew Stanton', 2003, 'animation', 'others', 100, 'Australia', 'Alexander Gould', 7.1, 126).
+film('Toy Story', 'John Lasseter', 1995, 'animation', 'others', 80, 'USA', 'Tom Hanks', 7.4, 124).
+film('Space Jam', 'Joe Pytka', 1996, 'animation', 'others', 88, 'USA', 'Michael Jordan', 6.9, 100).
+film('South Park: Bigger, Longer & Uncut', 'Trey Parker', 1999, 'animation', 'others', 81, 'USA', 'Trey Parker', 7.2, 59).
+film('Tarzan', 'Chris Buck', 1999, 'animation', 'others', 88, 'USA', 'Tony Goldwyn', 7.4, 42).
+film('The Snow Queen', 'Vladlen Barbe', 2012, 'animation', 'others', 76, 'Russia', 'Ivan Okhlobystin', 5.4, 18).
+film('The Fixies: Top Secret', 'Vasiko Bedoshvili', 2017, 'animation', 'others', 80, 'Russia', 'Larisa Brokhman', 5.5, 26).
+film('The Last Warrior', 'Dmitriy Dyachenko', 2017, 'animation', 'others', 114, 'Russia', 'Viktor Khorinyak', 6.6, 22).
+film('Prince Vladimir', 'Yuriy Kulakov', 2006, 'animation', 'others', 84, 'Russia', 'Vyacheslav Nevinnyy', 6.9, 32).
+
+
+/** ROMANCE */
+film('Chocolat', 'Lasse Hallstrom', 2000, 'romance', 'others', 117, 'USA', 'Juliette Binoche', 7.5, 164).
+film('Step Up', 'Anne Fletcher', 2006, 'romance', 'others', 104, 'USA', 'Channing Tatum', 7.0, 174).
+film('About Time', 'Richard Curtis', 2013, 'romance', 'others', 119, 'United Kingdom', 'Domhnall Gleeson', 7.6, 100).
+film('500 Days of Summer', 'Marc Webb', 2009, 'romance', 'others', 95, 'USA', 'Joseph Gordon-Levitt', 7.2, 99).
+film('Something\'s Gotta Give', 'Nancy Meyers', 2003, 'romance', 'others', 128, 'USA', 'Jack Nicholson', 7.0, 102).
+film('Amelie', 'Jean-Pierre Jeunet', 2001, 'romance', 'others', 129, 'France', 'Audrey Tautou', 7.6, 203).
+film('Love Actually', 'Richard Curtis', 2003, 'romance', 'others', 135, 'USA', 'Colin Firth', 7.7, 173).
+film('Bridget Jones\'s Diary', 'Sharon Maguire', 2001, 'romance', 'others', 97, 'United Kingdom', 'Renee Zellweger', 6.9, 204).
+film('Hipsters', 'Valeriy Todorovskiy', 2008, 'romance', 'others', 125, 'Russia', 'Anton Shagin', 6.4, 57).
+film('The Irony of Fate 2', 'Timur Bekmambetov', 2007, 'romance', 'others', 125, 'Russia', 'Konstantin Khabenskiy', 6.1, 47).
+film('Burnt by the Sun', 'Nikita Mikhalkov', 1994, 'romance', 'others', 135, 'Russia', 'Ingeborga Dapkunaite', 7.9, 33).
+
+
+/** HORROR */
+film('The Shining', 'Stanley Kubrick', 1980, 'horror', 'psychopaths', 146, 'USA', 'Jack Nicholson', 7.9, 293).
+film('I Am Legend', 'Francis Lawrence', 2007, 'horror', 'monsters', 101, 'Australia', 'Will Smith', 7.5, 344).
+film('Interview with the Vampire', 'Neil Jordan', 1994, 'horror', 'monsters', 123, 'USA', 'Brad Pitt', 7.8, 243).
+film('Alien', 'Ridley Scott', 1979, 'horror', 'monsters', 117, 'USA', 'Sigourney Weaver', 7.9, 211).
+film('The Ring', 'Gore Verbinski', 2002, 'horror', 'ghosts', 115, 'Japan', 'Naomi Watts', 7.0, 173).
+film('Saw', 'James Wan', 2004, 'horror', 'psychopaths', 103, 'Australia', 'Tobin Bell', 6.7, 172).
+film('Final Destination', 'James Wong', 2000, 'horror', 'others', 98, 'Canada', 'Devon Sawa', 6.6, 158).
+film('The Ninth Gate', 'Roman Polanski', 1999, 'horror', 'others', 133, 'France', 'Johnny Depp', 7.3, 148).
+film('The Conjuring', 'James Wan', 2013, 'horror', 'ghosts', 112, 'USA', 'Vera Farmiga', 7.4, 116).
+film('The Orphanage', 'Juan Antonio Bayona', 2007, 'horror', 'ghosts', 105, 'Spain', 'Belen Rueda', 7.0, 119).
+film('The Rite', 'Mikael Hafstrom', 2011, 'horror', 'ghosts', 114, 'USA', 'Anthony Hopkins', 6.8, 84).
+film('Night Watch', 'Timur Bekmambetov', 2004, 'horror', 'vampires', 114, 'Russia', 'Konstantin Khabenskiy', 6.4, 69).
+film('Day Watch', 'Timur Bekmambetov', 2006, 'horror', 'vampires', 132, 'Russia', 'Konstantin Khabenskiy', 6.4, 67).
+film('Gogol. The Beginning', 'Egor Baranov', 2017, 'horror', 'others', 100, 'Russia', 'Alexander Petrov', 5.8, 46).
+
+
+/** ACTORS */
+actor('Tom Hanks').
+actor('Ben Foster').
+actor('Nicole Kidman').
+actor('Adrien Brody').
+actor('Brad Pitt').
+actor('Jared Leto').
+actor('Javier Bardem').
+actor('Natalie Portman').
+actor('Russell Crowe').
+actor('Bruce Willis').
+actor('Morgan Freeman').
+actor('Leonardo DiCaprio').
+actor('Keanu Reeves').
+actor('Christian Bale').
+actor('Anthony Hopkins').
+actor('Jim Carrey').
+actor('Macaulay Culkin').
+actor('Meryl Streep').
+actor('Yuri Yakovlev').
+actor('Alexei Smirnov').
+actor('Yevgeny Leonov').
+actor('Elijah Wood').
+actor('Robert Downey Jr').
+actor('Daniel Radcliffe').
+actor('Martin Freeman').
+actor('Johnny Depp').
+actor('Michael J. Fox').
+actor('Michal Zebrowski').
+actor('Viktor Khorinyak').
+actor('Vyacheslav Nevinnyy').
+actor('Sigourney Weaver').
+actor('Naomi Watts').
+actor('Tobin Bell').
+actor('Devon Sawa').
+actor('Vera Farmiga').
+actor('Belen Rueda').
+actor('Thomas Kretschmann').
+actor('Konstantin Khabenskiy').
+actor('Alesya Krasnova').
+actor('Vera Alentova').
+actor('Anton Shagin').
+actor('Ingeborga Dapkunaite').
+actor('Francois Cluzet').
+actor('Domhnall Gleeson').
+actor('Joseph Gordon-Levitt').
+actor('Colin Firth').
+actor('Audrey Tautou').
+actor('Anton Shagin').
+actor('Alexander Kaidanovsky').
+actor('Alexander Petrov').
+actor(X) :- not(actress(X)).
